ae1c4e3256794d2704cda5bc8164a9836af10805
Kirsten Schumy <ksschumy@amazon.com>
2018-12-03 19:39:18 -0800

    github/ratelimit.go: update comment

--- pkg/github-query/github/ratelimit.go
+++ pkg/github-query/github/ratelimit.go
@@ -5,7 +5,7 @@ import "time"
 const RateLimitAsString = "rateLimit{cost,limit,nodeCount,remaining,resetAt}"
 
 type rateLimit struct {
-	Cost      int       `json:"cost"`      // how many points the gh cost
+	Cost      int       `json:"cost"`      // how many points the query cost
 	Limit     int       `json:"limit"`     // how many points can be used in rate limit window (60 minutes)
 	NodeCount int       `json:"nodeCount"` // number of nodes results were in this gh
 	Remaining int       `json:"remaining"` // how many point are remaining in the rate limit
